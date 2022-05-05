d5b8d6e51ccd7f3ed560210a0b9c0b8e784cfd0a
martijn.benjamin <martijn.teigeler@gmail.com>
2022-05-05 15:12:10 +0200

    Refactor template to nylon pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,8 +41,8 @@ use pallet_transaction_payment::CurrencyAdapter;
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
-/// Import the template pallet.
-pub use pallet_template;
+/// Import the Nylon pallet.
+pub use pallet_nylon;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -263,8 +263,8 @@ impl pallet_sudo::Config for Runtime {
 	type Call = Call;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+/// Configure the pallet-nylon in pallets/template.
+impl pallet_nylon::Config for Runtime {
 	type Event = Event;
 }
 
@@ -283,8 +283,8 @@ construct_runtime!(
 		Balances: pallet_balances,
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
-		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
+		// Include the custom logic from the pallet-nylon in the runtime.
+		TemplateModule: pallet_nylon,
 	}
 );
 
@@ -329,7 +329,7 @@ mod benches {
 		[frame_system, SystemBench::<Runtime>]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
-		[pallet_template, TemplateModule]
+		[pallet_nylon, TemplateModule]
 	);
 }
 
